726319a14b0873099403d7f9e9040e349e132b13
BaoFlutter <baoflutter@gmail.com>
2021-08-17 14:39:39 +0700

    add dictionary

---
+++ dictionary/lib/models/word_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class WordModel {
+  int? word_id;
+  String? word;
+  String? pronounce;
+  String? meaning;
+
+  WordModel({ @required this.word_id, @required this.word, @required this.pronounce, @required this.meaning });
+
+  // From Json
+  WordModel.fromJson(json):
+        word_id = json['word_id'],
+        word =  json['word'],
+        pronounce = json['pronounce'],
+        meaning= json['meaning'];
+
+
+  // to Json
+  toJson()
+  {
+    return {
+      "word_id": this.word_id,
+      "word": this.word,
+      "pronounce": this.pronounce,
+      "meaning": this.meaning
+    };
+
+  }
+}
